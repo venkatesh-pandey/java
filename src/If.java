1fdf8efa52cd09de9891caf1a5167a1be393b935
Venkatesh Pandey <venlaptop@gmail.com>
2023-01-01 12:29:08 +0530

    3ed

---
+++ src/If.java
@@ -0,0 +1,9 @@
+public class If {
+    public static void main(String[] args) {
+        int x =10;
+        int y= 12;
+        if(x+y>20){
+            System.out.println("x+y is greater then 20.");
+        }
+    }
+}
